a165d517d57e89c115aa5b98a7d9e9dd04578bb5
ChanwooCho <41471223+ChanwooCho@users.noreply.github.com>
2024-10-01 22:42:59 +0900

    Add files via upload

---
+++ client.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <cstring>
+#include <cstdlib>  // for atoi
+
+#define DATA_SIZE 20480  // 20KB
+#define ITERATIONS 160
+
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        std::cerr << "Usage: client <ip address:port>" << std::endl;
+        return -1;
+    }
+
+    // Split the input argument into IP address and port
+    std::string input(argv[1]);
+    std::size_t colon_pos = input.find(':');
+    if (colon_pos == std::string::npos) {
+        std::cerr << "Invalid argument format. Use: <ip address:port>" << std::endl;
+        return -1;
+    }
+
+    std::string ip_address = input.substr(0, colon_pos);
+    int port = std::atoi(input.substr(colon_pos + 1).c_str());
+
+    int sock = 0;
+    struct sockaddr_in serv_addr;
+    char buffer[DATA_SIZE] = {0};
+    char data[DATA_SIZE] = {0};
+
+    // Fill the data buffer with some data
+    memset(data, 'A', DATA_SIZE);
+
+    // Create socket
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        std::cerr << "Socket creation error" << std::endl;
+        return -1;
+    }
+
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(port);
+
+    // Convert IPv4 and IPv6 addresses from text to binary form
+    if (inet_pton(AF_INET, ip_address.c_str(), &serv_addr.sin_addr) <= 0) {
+        std::cerr << "Invalid address/ Address not supported" << std::endl;
+        return -1;
+    }
+
+    // Connect to server
+    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+        std::cerr << "Connection Failed" << std::endl;
+        return -1;
+    }
+
+    std::cout << "Connected to server at " << ip_address << ":" << port << std::endl;
+
+    for (int i = 0; i < ITERATIONS; ++i) {
+        // Send 20KB data to server
+        if (send(sock, data, DATA_SIZE, 0) < 0) {
+            std::cerr << "Send failed" << std::endl;
+            break;
+        }
+        std::cout << "Sent 20KB to server: Iteration " << i + 1 << std::endl;
+
+        // Receive 20KB data from server
+        if (read(sock, buffer, DATA_SIZE) < 0) {
+            std::cerr << "Read failed" << std::endl;
+            break;
+        }
+        std::cout << "Received 20KB from server: Iteration " << i + 1 << std::endl;
+    }
+
+    // Close socket
+    close(sock);
+    std::cout << "Connection closed" << std::endl;
+
+    return 0;
+}
---
+++ server.cpp
@@ -0,0 +1,90 @@
+#include <iostream>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <cstring>
+#include <cstdlib>  // For atoi()
+
+#define DATA_SIZE 20480  // 20KB
+#define ITERATIONS 160
+
+int main(int argc, char* argv[]) {
+    if (argc != 2) {
+        std::cerr << "Usage: server <port>" << std::endl;
+        return -1;
+    }
+
+    int port = atoi(argv[1]);  // Convert the port argument to an integer
+
+    int server_fd, new_socket;
+    struct sockaddr_in address;
+    int opt = 1;
+    int addrlen = sizeof(address);
+    char buffer[DATA_SIZE] = {0};
+    char data[DATA_SIZE] = {0};
+
+    // Create socket file descriptor
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+        std::cerr << "Socket failed" << std::endl;
+        return -1;
+    }
+
+    // Attach socket to the port
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) {
+        std::cerr << "setsockopt failed" << std::endl;
+        close(server_fd);
+        return -1;
+    }
+    
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY; // Bind to any local IP address
+    address.sin_port = htons(port);       // Use the port passed as an argument
+
+    // Bind the socket to the network address and port
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
+        std::cerr << "Bind failed" << std::endl;
+        close(server_fd);
+        return -1;
+    }
+
+    // Listen for incoming connections
+    if (listen(server_fd, 3) < 0) {
+        std::cerr << "Listen failed" << std::endl;
+        close(server_fd);
+        return -1;
+    }
+
+    std::cout << "Waiting for connection on port " << port << "..." << std::endl;
+
+    // Accept incoming connection
+    if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen)) < 0) {
+        std::cerr << "Accept failed" << std::endl;
+        close(server_fd);
+        return -1;
+    }
+
+    std::cout << "Connection established with client." << std::endl;
+
+    for (int i = 0; i < ITERATIONS; ++i) {
+        // Receive 20KB data from client
+        if (read(new_socket, buffer, DATA_SIZE) < 0) {
+            std::cerr << "Read failed" << std::endl;
+            break;
+        }
+        std::cout << "Received 20KB from client: Iteration " << i + 1 << std::endl;
+
+        // Send 20KB data back to client
+        if (send(new_socket, data, DATA_SIZE, 0) < 0) {
+            std::cerr << "Send failed" << std::endl;
+            break;
+        }
+        std::cout << "Sent 20KB back to client: Iteration " << i + 1 << std::endl;
+    }
+
+    // Close sockets
+    close(new_socket);
+    close(server_fd);
+    std::cout << "Connection closed" << std::endl;
+
+    return 0;
+}
