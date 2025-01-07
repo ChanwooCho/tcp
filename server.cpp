97db7e4bd5873b258fd335f16ed6ef264c64c04d
ChanwooCho <41471223+ChanwooCho@users.noreply.github.com>
2025-01-07 19:57:37 +0900

    Update server.cpp

--- server.cpp
+++ server.cpp
@@ -152,16 +152,16 @@ int main(int argc, char* argv[]) {
         for (int i = 0; i < iterations; ++i) {
             // After reading from all clients, send data back to all clients
             for (int client_socket : client_sockets) {
-                // ssize_t bytes_sent = send(client_socket, data, data_size, 0);
+                ssize_t bytes_sent = send(client_socket, data, data_size, 0);
                 // printf("bytes_sent  = %d\n", bytes_sent);
-                send_all(client_socket, data, data_size);
+                // send_all(client_socket, data, data_size);
             }
 
             // Read from all connected clients
             for (int client_socket : client_sockets) {
-                // ssize_t bytes_received = read(client_socket, buffer, data_size);
+                ssize_t bytes_received = read(client_socket, buffer, data_size);
                 // printf("bytes_received = %d\n", bytes_received);
-                read_all(client_socket, buffer, data_size);
+                // read_all(client_socket, buffer, data_size);
             }
         }
         interval = timeUs() - before;
