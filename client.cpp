7c78c8d5426f1f5104b56ad9a03a31603f205f23
ChanwooCho <41471223+ChanwooCho@users.noreply.github.com>
2025-01-07 14:12:31 +0900

    Update client.cpp

--- client.cpp
+++ client.cpp
@@ -11,6 +11,35 @@ unsigned long timeUs() {
     return te.tv_sec * 1000000LL + te.tv_usec;
 }
 
+ssize_t read_all(int sock, char* buffer, size_t size) {
+    size_t total_read = 0;
+    while (total_read < size) {
+        ssize_t bytes_read = read(sock, buffer + total_read, size - total_read);
+        if (bytes_read < 0) {
+            perror("Read error");
+            return -1;
+        } else if (bytes_read == 0) {
+            // Connection closed
+            break;
+        }
+        total_read += bytes_read;
+    }
+    return total_read;
+}
+
+ssize_t send_all(int sock, const char* data, size_t size) {
+    size_t total_sent = 0;
+    while (total_sent < size) {
+        ssize_t bytes_sent = send(sock, data + total_sent, size - total_sent, 0);
+        if (bytes_sent < 0) {
+            perror("Send error");
+            return -1;
+        }
+        total_sent += bytes_sent;
+    }
+    return total_sent;
+}
+
 int main(int argc, char *argv[]) {
     if (argc != 4) {
         std::cerr << "Usage: client <data_size(KB)> <# of decoders> <ip_address:port>" << std::endl;
@@ -70,47 +99,37 @@ int main(int argc, char *argv[]) {
     std::cout << "Connected to server at " << ip_address << ":" << port << std::endl;
     
     // 기존 코드
-    for (int e = 0; e < 50; ++e) {
-        for (int i = 0; i < iterations; ++i) {
-            // Receive data_size KB data from server
-            ssize_t read_size = read(sock, buffer, data_size);
-            // printf("read = %d\n", read_size); 
-            
-            // Send data_size KB data to server
-            ssize_t send_size = send(sock, data, data_size, 0);
-            // printf("send = %d\n", send_size);
-        }
-    }
-
-    // int totalBytesToRead;
-    // int bytesRead;
-    // int cnt;
-    // unsigned int before;
-    // unsigned int interval;
     // for (int e = 0; e < 50; ++e) {
     //     for (int i = 0; i < iterations; ++i) {
-    //         totalBytesToRead = data_size;
-    //         bytesRead = 0;
-    //         cnt = 0;
     //         // Receive data_size KB data from server
-    //         before = timeUs();
-    //         while (bytesRead < totalBytesToRead) {
-    //             ssize_t ret = read(sock, buffer + bytesRead, totalBytesToRead - bytesRead);
-    //             interval = timeUs() - before;
-    //             printf("sliced_data = %d, time = %dus\n", ret, interval); 
-    //             before = timeUs();
-    //             if (ret <= 0) {
-    //                 // Handle error or EOF
-    //                 break;
-    //             }
-    //             cnt += 1;
-    //             bytesRead += ret;
-    //         }
-    //         printf("=====cnt = %d, bytes = %d\n", cnt, bytesRead);
+    //         ssize_t read_size = read(sock, buffer, data_size);
+    //         // printf("read = %d\n", read_size); 
+            
     //         // Send data_size KB data to server
-    //         send(sock, data, data_size, 0);
+    //         ssize_t send_size = send(sock, data, data_size, 0);
+    //         // printf("send = %d\n", send_size);
     //     }
     // }
+
+
+    for (int e = 0; e < 50; ++e) {
+        for (int i = 0; i < iterations; ++i) {
+            ssize_t bytes_received = read_all(sock, buffer, data_size);
+            if (bytes_received != data_size) {
+                std::cerr << "Failed to receive full data_size bytes" << std::endl;
+                // Handle error (e.g., retry, exit, etc.)
+                break;
+            }
+            
+            // Send data_size bytes to server
+            ssize_t bytes_sent = send_all(sock, data, data_size);
+            if (bytes_sent != data_size) {
+                std::cerr << "Failed to send full data_size bytes" << std::endl;
+                // Handle error
+                break;
+            }
+        }
+    }
 
     // Close socket
     close(sock);
