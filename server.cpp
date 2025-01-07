d99ba05f2e322b536ea9c5c969cfaa99302b3e47
ChanwooCho <41471223+ChanwooCho@users.noreply.github.com>
2025-01-07 20:30:17 +0900

    Update server.cpp

--- server.cpp
+++ server.cpp
@@ -162,6 +162,7 @@ int main(int argc, char* argv[]) {
             for (int client_socket : client_sockets) {
                 size_t bytes_sent = send(client_socket, data, data_size, 0);
                 // send_all(client_socket, data, data_size);
+                printf("bytes_sent = %d\n", bytes_sent);
             }
 
             read_all(client_socket, buffer, data_size);
@@ -169,6 +170,7 @@ int main(int argc, char* argv[]) {
             for (int client_socket : client_sockets) {
                 size_t bytes_received = read(client_socket, buffer, data_size);
                 // read_all(client_socket, buffer, data_size);
+                printf("bytes_received = %d\n", bytes_received);
             }
         }
         interval = timeUs() - before;
