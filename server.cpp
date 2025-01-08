7139c58c13560525244c4d1b95e494ce465460ca
ChanwooCho <41471223+ChanwooCho@users.noreply.github.com>
2025-01-08 16:13:10 +0900

    Update server.cpp

--- server.cpp
+++ server.cpp
@@ -176,8 +176,8 @@ int main(int argc, char* argv[]) {
 
             for (int client_socket : client_sockets) {
                 before2 = timeUs();
-                size_t bytes_read = read(client_socket, buffer, data_size);
-                // size_t bytes_read = read_all(client_socket, buffer, data_size, e, i);
+                // size_t bytes_read = read(client_socket, buffer, data_size);
+                size_t bytes_read = read_all(client_socket, buffer, data_size, e, i);
                 interval2 = timeUs() - before2;
                 // printf("iteration %d decoder %d: bytes_read = %d, interval_read = %dus\n", e, i, bytes_read, interval2);
             }
