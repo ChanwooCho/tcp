8c5af67d1dcaf10c9d7291b4b1a9752bfce3a588
ChanwooCho <41471223+ChanwooCho@users.noreply.github.com>
2025-01-08 13:33:46 +0900

    Update server.cpp

--- server.cpp
+++ server.cpp
@@ -15,6 +15,44 @@ unsigned long timeUs() {
     return te.tv_sec * 1000000LL + te.tv_usec;
 }
 
+ssize_t read_all(int sock, char* buffer, size_t size, int e, int d) {
+    size_t total_read = 0;
+    unsigned int before;
+    unsigned int interval;
+
+    while (total_read < size) {
+        before = timeUs();
+        ssize_t bytes_read = read(sock, buffer + total_read, size - total_read);
+        interval = timeUs() - before;
+        // printf("iteration %d decoder %d: bytes_read = %d, interval = %dus\n", e, d, bytes_read, interval);
+
+        
+        if (bytes_read < 0) {
+            perror("Read error");
+            return -1;
+        } else if (bytes_read == 0) {
+            // Connection closed
+            break;
+        }
+        total_read += bytes_read;
+    }
+    return total_read;
+}
+
+ssize_t send_all(int sock, const char* data, size_t size, int e, int d) {
+    size_t total_sent = 0;
+    while (total_sent < size) {
+        ssize_t bytes_sent = send(sock, data + total_sent, size - total_sent, 0);
+        if (bytes_sent < 0) {
+            perror("Send error");
+            return -1;
+        }
+        total_sent += bytes_sent;
+    }
+    // printf("==============================================================\n");
+    return total_sent;
+}
+
 int main(int argc, char* argv[]) {
     if (argc != 5) {
         std::cerr << "Usage: server <data_size(KB)> <# of decoders> <# of clients> <port>" << std::endl;
@@ -129,7 +167,8 @@ int main(int argc, char* argv[]) {
             memset(data, 'A' + i % 26, data_size);
             for (int client_socket : client_sockets) {
                 before2 = timeUs();
-                size_t bytes_send = send(client_socket, data, data_size, 0);
+                // size_t bytes_send = send(client_socket, data, data_size, 0);
+                size_t bytes_send = send_all(client_socket, data, data_size);
                 interval2 = timeUs() - before2;
                 // printf("iteration %d decoder %d: bytes_send = %d, interval_send = %dus\n", e, i, bytes_send, interval2);
             }
@@ -138,7 +177,8 @@ int main(int argc, char* argv[]) {
 
             for (int client_socket : client_sockets) {
                 before2 = timeUs();
-                size_t bytes_read = read(client_socket, buffer, data_size);
+                // size_t bytes_read = read(client_socket, buffer, data_size);
+                size_t bytes_read = read_all(client_socket, buffer, data_size);
                 interval2 = timeUs() - before2;
                 printf("iteration %d decoder %d: bytes_read = %d, interval_read = %dus\n", e, i, bytes_read, interval2);
             }
