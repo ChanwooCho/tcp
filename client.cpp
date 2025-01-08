210af44d8602a667f2d877ed9c9cea1bb83c8766
ChanwooCho <41471223+ChanwooCho@users.noreply.github.com>
2025-01-08 10:47:36 +0900

    Update client.cpp

--- client.cpp
+++ client.cpp
@@ -20,7 +20,7 @@ ssize_t read_all(int sock, char* buffer, size_t size, int e, int d) {
         before = timeUs();
         ssize_t bytes_read = read(sock, buffer + total_read, size - total_read);
         interval = timeUs() - before;
-        printf("iteration %d decoder %d: bytes_read = %d, interval = %dus\n", e, d, bytes_read, interval);
+        // printf("iteration %d decoder %d: bytes_read = %d, interval = %dus\n", e, d, bytes_read, interval);
 
         
         if (bytes_read < 0) {
@@ -45,7 +45,7 @@ ssize_t send_all(int sock, const char* data, size_t size, int e, int d) {
         }
         total_sent += bytes_sent;
     }
-    printf("==============================================================\n");
+    // printf("==============================================================\n");
     return total_sent;
 }
 
