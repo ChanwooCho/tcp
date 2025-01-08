efdeec9b848a152aee572afd2a31a32c76fbbf77
ChanwooCho <41471223+ChanwooCho@users.noreply.github.com>
2025-01-08 15:52:40 +0900

    Update server.cpp

--- server.cpp
+++ server.cpp
@@ -166,8 +166,8 @@ int main(int argc, char* argv[]) {
             memset(data, 'A' + i % 26, data_size);
             for (int client_socket : client_sockets) {
                 before2 = timeUs();
-                size_t bytes_send = send(client_socket, data, data_size, 0);
-                // size_t bytes_send = send_all(client_socket, data, data_size, e, i);
+                // size_t bytes_send = send(client_socket, data, data_size, 0);
+                size_t bytes_send = send_all(client_socket, data, data_size, e, i);
                 interval2 = timeUs() - before2;
                 // printf("iteration %d decoder %d: bytes_send = %d, interval_send = %dus\n", e, i, bytes_send, interval2);
             }
@@ -176,8 +176,8 @@ int main(int argc, char* argv[]) {
 
             for (int client_socket : client_sockets) {
                 before2 = timeUs();
-                // size_t bytes_read = read(client_socket, buffer, data_size);
-                size_t bytes_read = read_all(client_socket, buffer, data_size, e, i);
+                size_t bytes_read = read(client_socket, buffer, data_size);
+                // size_t bytes_read = read_all(client_socket, buffer, data_size, e, i);
                 interval2 = timeUs() - before2;
                 // printf("iteration %d decoder %d: bytes_read = %d, interval_read = %dus\n", e, i, bytes_read, interval2);
             }
