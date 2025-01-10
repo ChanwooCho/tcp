a5a07370efcaf1a9744cc83c408630c852de6c09
ChanwooCho <41471223+ChanwooCho@users.noreply.github.com>
2025-01-10 12:02:22 +0900

    Update client.cpp

--- client.cpp
+++ client.cpp
@@ -5,6 +5,15 @@
 #include <cstring>
 #include <cstdlib>  // for atoi
 
+int flag = 1;
+if (setsockopt(sock, IPPROTO_TCP, TCP_NODELAY, &flag, sizeof(flag)) < 0) {
+    perror("setsockopt(TCP_NODELAY) failed");
+}
+
+int buff_size = 1 * 1024 * 1024; // 1MB, for example
+setsockopt(sock, SOL_SOCKET, SO_SNDBUF, &buff_size, sizeof(buff_size));
+setsockopt(sock, SOL_SOCKET, SO_RCVBUF, &buff_size, sizeof(buff_size));
+
 unsigned long timeUs() {
     struct timeval te; 
     gettimeofday(&te, NULL);
