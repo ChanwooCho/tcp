3da9af018c0ed83b492dbc85050acc4119b3468b
ChanwooCho <41471223+ChanwooCho@users.noreply.github.com>
2025-01-13 14:45:48 +0900

    Update server.cpp

--- server.cpp
+++ server.cpp
@@ -22,10 +22,10 @@ ssize_t read_all(int sock, char* buffer, size_t size, int e, int d) {
     unsigned int interval;
 
     while (total_read < size) {
-        // before = timeUs();
+        before = timeUs();
         ssize_t bytes_read = read(sock, buffer + total_read, size - total_read);
-        // interval = timeUs() - before;
-        // printf("iteration %d decoder %d: bytes_read = %d, interval = %dus\n", e, d, bytes_read, interval)
+        interval = timeUs() - before;
+        printf("iteration %d decoder %d: bytes_read = %d, interval = %dus\n", e, d, bytes_read, interval)
         if (bytes_read < 0) {
             perror("Read error");
             return -1;
