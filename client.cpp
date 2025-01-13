a9452248e63ca99227bf9af04ec4ed6b0c099a9e
ChanwooCho <41471223+ChanwooCho@users.noreply.github.com>
2025-01-13 20:15:19 +0900

    Update client.cpp

--- client.cpp
+++ client.cpp
@@ -164,9 +164,10 @@ int main(int argc, char *argv[]) {
             sum_interval3 += interval2;
 
             // printf("current recieve data = %c\n", buffer[data_size - 1]);
+            interval = timeUs() - before;
+            printf("iteration %d decoder %d: interval = %dus\n", e, i, interval);
         }
-        interval = timeUs() - before;
-        printf("iteration %d decoder %d: interval = %dus\n", e, i, interval);
+
         printf("==============================================================\n");
         // printf("iteration %d's read time = %d ms, send time = %d ms\n", e, sum_interval2 / 1000, sum_interval3 / 1000);
         // printf("minimum latency = %dus\n", min_latency); 
