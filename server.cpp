1adda02f2c8e795f76b7c9821ff2eade314bf271
ChanwooCho <41471223+ChanwooCho@users.noreply.github.com>
2025-01-13 15:17:08 +0900

    Update server.cpp

--- server.cpp
+++ server.cpp
@@ -22,10 +22,10 @@ ssize_t read_all(int sock, char* buffer, size_t size, int e, int d) {
     unsigned int interval;
 
     while (total_read < size) {
-        // before = timeUs();
+        before = timeUs();
         ssize_t bytes_read = read(sock, buffer + total_read, size - total_read);
-        // interval = timeUs() - before;
-        // printf("iteration %d decoder %d: bytes_read = %d, interval = %dus\n", e, d, bytes_read, interval);
+        interval = timeUs() - before;
+        printf("iteration %d decoder %d: bytes_read = %d, interval = %dus\n", e, d, bytes_read, interval);
         if (bytes_read < 0) {
             perror("Read error");
             return -1;
@@ -43,17 +43,17 @@ ssize_t send_all(int sock, const char* data, size_t size, int e, int d) {
     unsigned int before;
     unsigned int interval;
     while (total_sent < size) {
-        // before = timeUs();
+        before = timeUs();
         ssize_t bytes_sent = send(sock, data + total_sent, size - total_sent, 0);
-        // interval = timeUs() - before;
-        // printf("iteration %d decoder %d: bytes_sent = %d, interval = %dus\n", e, d, bytes_sent, interval);
+        interval = timeUs() - before;
+        printf("iteration %d decoder %d: bytes_sent = %d, interval = %dus\n", e, d, bytes_sent, interval);
         if (bytes_sent < 0) {
             perror("Send error");
             return -1;
         }
         total_sent += bytes_sent;
     }
-    // printf("==============================================================\n");
+    printf("==============================================================\n");
     return total_sent;
 }
 
