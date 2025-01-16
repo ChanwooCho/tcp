111389ca11f508cbcfc2d1aa5b8c449917e34616
ChanwooCho <41471223+ChanwooCho@users.noreply.github.com>
2025-01-16 19:51:10 +0900

    Update split_server.cpp

--- split_server.cpp
+++ split_server.cpp
@@ -180,12 +180,12 @@ int main(int argc, char* argv[]) {
             memset(data, 'A' + i % 26, data_size);
             before1 = timeUs();
             for (int client_socket : client_sockets) {
-                for (int j = 0; j < 7; j++){
-                std::this_thread::sleep_for(std::chrono::microseconds(234));
-                send_all(client_socket, data, 1448, e, i);
+                for (int j = 0; j < 3; j++){
+                std::this_thread::sleep_for(std::chrono::microseconds(234 * 2));
+                send_all(client_socket, data, 1448 * 2, e, i);
                 }
-                std::this_thread::sleep_for(std::chrono::microseconds(17));
-                send_all(client_socket, data, 104, e, i);
+                std::this_thread::sleep_for(std::chrono::microseconds(234 + 17));
+                send_all(client_socket, data, 1448 + 104, e, i);
             }
             // sum_interval2 += interval2;
             // Read from all connected clients
