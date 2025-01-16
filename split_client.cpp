28cb82f8d2540fd46f917e8c075758d75c6f0101
ChanwooCho <41471223+ChanwooCho@users.noreply.github.com>
2025-01-16 16:34:41 +0900

    Update split_client.cpp

--- split_client.cpp
+++ split_client.cpp
@@ -17,17 +17,23 @@ unsigned long timeUs() {
     gettimeofday(&te, NULL);
     return te.tv_sec * 1000000LL + te.tv_usec;
 }
-
+unsigned int min_latency;
 ssize_t read_all(int sock, char* buffer, size_t size, int e, int d) {
     size_t total_read = 0;
     unsigned int before;
     unsigned int interval;
-
+    unsigned int is_first = 1;
+    
     while (total_read < size) {
         before = timeUs();
         ssize_t bytes_read = read(sock, buffer + total_read, size - total_read);
         interval = timeUs() - before;
-        // printf("iteration %d decoder %d: bytes_read = %d, interval = %dus\n", e, d, bytes_read, interval);
+        if (min_latency > interval && is_first) {
+            min_latency = interval;
+        }
+        is_first = 0;
+        printf("iteration %d decoder %d: bytes_read = %d, interval = %dus\n", e, d, bytes_read, interval);
+
         if (bytes_read < 0) {
             perror("Read error");
             return -1;
@@ -48,120 +54,93 @@ ssize_t send_all(int sock, const char* data, size_t size, int e, int d) {
         before = timeUs();
         ssize_t bytes_sent = send(sock, data + total_sent, size - total_sent, 0);
         interval = timeUs() - before;
-        // printf("iteration %d decoder %d: bytes_sent = %d, interval = %dus\n", e, d, bytes_sent, interval);
+        printf("iteration %d decoder %d: bytes_send = %d, interval = %dus\n", e, d, bytes_sent, interval);
         if (bytes_sent < 0) {
             perror("Send error");
             return -1;
         }
         total_sent += bytes_sent;
     }
+    // printf("==============================================================\n");
     return total_sent;
 }
 
-int main(int argc, char* argv[]) {
-    if (argc != 5) {
-        std::cerr << "Usage: server <data_size(KB)> <# of decoders> <# of clients> <port>" << std::endl;
+int main(int argc, char *argv[]) {
+    if (argc != 4) {
+        std::cerr << "Usage: client <data_size(KB)> <# of decoders> <ip_address:port>" << std::endl;
         return -1;
     }
 
-    // Extract command-line arguments
-    int data_size = atoi(argv[1]) * 1024; // Convert the data size argument to an integer
-    int iterations = atoi(argv[2]) * 2;   // Convert the iterations argument to an integer
-    int num_clients = atoi(argv[3]);      // Number of clients to wait for
-    int port = atoi(argv[4]);             // Convert the port argument to an integer
+    // Parse data_size and iterations
+    int data_size = std::atoi(argv[1]) * 1024; // Bytes
+    int iterations = std::atoi(argv[2]) * 2; // attention layer + feedforward layer
 
-    int server_fd, new_socket;
-    struct sockaddr_in address;
-    int opt = 1;
-    int addrlen = sizeof(address);
-    fd_set read_fds, write_fds;
-    std::vector<int> client_sockets;
+    // Split the IP address and port
+    std::string input(argv[3]);
+    std::size_t colon_pos = input.find(':');
+    if (colon_pos == std::string::npos) {
+        std::cerr << "Invalid argument format. Use: <ip address:port>" << std::endl;
+        return -1;
+    }
 
-    // Dynamically allocate buffer and data arrays based on the specified data size
-    char* buffer = new char[data_size];
-    char* data = new char[data_size];
+    std::string ip_address = input.substr(0, colon_pos);
+    int port = std::atoi(input.substr(colon_pos + 1).c_str());
 
-    // Create socket file descriptor
-    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
-        std::cerr << "Socket failed" << std::endl;
+    int sock = 0;
+    struct sockaddr_in serv_addr;
+    char *buffer = new char[data_size];
+    char *data = new char[data_size];
+
+    // Create socket
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        std::cerr << "Socket creation error" << std::endl;
         delete[] buffer;
         delete[] data;
         return -1;
     }
+
     // disable nagle algorithm
     // int flag = 1;
-    // if (setsockopt(server_fd, IPPROTO_TCP, TCP_NODELAY, &flag, sizeof(flag)) < 0) {
+    // if (setsockopt(sock, IPPROTO_TCP, TCP_NODELAY, &flag, sizeof(flag)) < 0) {
     //     perror("setsockopt(TCP_NODELAY) failed");
     // }
     // int buff_size = 1 * 1024 * 1024; // 1MB, for example
-    // setsockopt(server_fd, SOL_SOCKET, SO_SNDBUF, &buff_size, sizeof(buff_size));
-    // setsockopt(server_fd, SOL_SOCKET, SO_RCVBUF, &buff_size, sizeof(buff_size));
+    // setsockopt(sock, SOL_SOCKET, SO_SNDBUF, &buff_size, sizeof(buff_size));
+    // setsockopt(sock, SOL_SOCKET, SO_RCVBUF, &buff_size, sizeof(buff_size));
 
-    // Attach socket to the port
-    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) {
-        std::cerr << "setsockopt failed" << std::endl;
-        close(server_fd);
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(port);
+
+    // Convert IPv4 and IPv6 addresses from text to binary form
+    if (inet_pton(AF_INET, ip_address.c_str(), &serv_addr.sin_addr) <= 0) {
+        std::cerr << "Invalid address/ Address not supported" << std::endl;
         delete[] buffer;
         delete[] data;
         return -1;
     }
 
-    address.sin_family = AF_INET;
-    address.sin_addr.s_addr = INADDR_ANY; // Bind to any local IP address
-    address.sin_port = htons(port);       // Use the port passed as an argument
-
-    // Bind the socket to the network address and port
-    if (bind(server_fd, (struct sockaddr*)&address, sizeof(address)) < 0) {
-        std::cerr << "Bind failed" << std::endl;
-        close(server_fd);
+    // Connect to server
+    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+        std::cerr << "Connection Failed" << std::endl;
         delete[] buffer;
         delete[] data;
         return -1;
     }
 
-    // Listen for incoming connections
-    if (listen(server_fd, 10) < 0) {
-        std::cerr << "Listen failed" << std::endl;
-        close(server_fd);
-        delete[] buffer;
-        delete[] data;
-        return -1;
-    }
-
-    std::cout << "Waiting for connections on port " << port << "..." << std::endl;
-
-    // Wait for the specified number of clients to connect
-    while (client_sockets.size() < num_clients) {
-        FD_ZERO(&read_fds);
-        FD_SET(server_fd, &read_fds);
-        int max_sd = server_fd;
-
-        // Use select() to wait for a new client connection
-        int activity = select(max_sd + 1, &read_fds, NULL, NULL, NULL);
-
-        if (activity < 0 && errno != EINTR) {
-            std::cerr << "Select error" << std::endl;
-            break;
-        }
-
-        // Check if there’s a new connection request
-        if (FD_ISSET(server_fd, &read_fds)) {
-            if ((new_socket = accept(server_fd, (struct sockaddr*)&address, (socklen_t*)&addrlen)) < 0) {
-                std::cerr << "Accept failed" << std::endl;
-                close(server_fd);
-                delete[] buffer;
-                delete[] data;
-                return -1;
-            }
-
-            std::cout << "New client connected." << std::endl;
-            client_sockets.push_back(new_socket); // Add new socket to the list
-        }
-
-        std::cout << "Waiting for " << num_clients << " clients. Currently connected clients: " << client_sockets.size() << std::endl;
-    }
-
-    std::cout << "Minimum " << num_clients << " clients connected. Starting main loop." << std::endl;
+    std::cout << "Connected to server at " << ip_address << ":" << port << std::endl;
+    
+    // 기존 코드
+    // for (int e = 0; e < 50; ++e) {
+    //     for (int i = 0; i < iterations; ++i) {
+    //         // Receive data_size KB data from server
+    //         ssize_t read_size = read(sock, buffer, data_size);
+    //         // printf("read = %d\n", read_size); 
+            
+    //         // Send data_size KB data to server
+    //         ssize_t send_size = send(sock, data, data_size, 0);
+    //         // printf("send = %d\n", send_size);
+    //     }
+    // }
 
     unsigned int before1;
     unsigned int before2;
