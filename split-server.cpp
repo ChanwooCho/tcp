f3ef107d4c633bbe5009ff843c3159b0b629542d
ChanwooCho <41471223+ChanwooCho@users.noreply.github.com>
2025-01-27 11:09:00 +0900

    Update split-server.cpp

--- split-server.cpp
+++ split-server.cpp
@@ -13,40 +13,6 @@
 #include <chrono> 
 #include <sched.h>
 
-// // For Android Core Affinity
-// #if defined(__ANDROID__)
-//     #if defined(__x86_64__)
-//     #define __NR_sched_setaffinity 203
-//     #elif defined(__arm__)
-//     #define __NR_sched_setaffinity 241
-//     #elif defined(__aarch64__)
-//     #define __NR_sched_setaffinity 122
-//     #endif
-//     #define CPU_SETSIZE 1024
-//     #define __NCPUBITS (8 * sizeof (unsigned long))
-//     typedef struct {
-//         unsigned long __bits[CPU_SETSIZE / __NCPUBITS];
-//     } cpu_set_t;
-
-//     void CPU_ZERO(cpu_set_t *set) {
-//         memset(set, 0, sizeof(cpu_set_t));
-//     }
-
-//     void CPU_SET(int cpu, cpu_set_t *set) {
-//         set->__bits[cpu / __NCPUBITS] |= (1UL << (cpu % __NCPUBITS));
-//     }
-
-//     // Define sched_setaffinity using syscall
-//     int sched_setaffinity(pid_t pid, size_t cpusetsize, const cpu_set_t *mask) {
-//         int result = syscall(__NR_sched_setaffinity, pid, cpusetsize, mask);
-//         if (result != 0) {
-//             errno = result;
-//             return -1;
-//         }
-//         return 0;
-//     }
-// #endif
-
 unsigned long timeUs() {
     struct timeval te; 
     gettimeofday(&te, NULL);
